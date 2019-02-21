9d8c7f82b80e8be80252a9538401b52021f10799
PerseusRipple <perseus.ripple@gmail.com>
2019-02-21 18:36:08 -0500

    added html, remember button, and map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,109 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    hue: 50,
+    saturation: 80,
+    lightness: 20,
+    colors: []
+  };
+
+  addColorToList = () => {
+    this.setState({
+      colors: this.state.colors.concat(
+        `hsl(${this.state.hue},${this.state.saturation}%,${
+          this.state.lightness
+        }%)`
+      )
+    });
+  };
+
+  updateHue = event => {
+    console.log(event.target.value);
+    this.setState({
+      hue: event.target.value
+    });
+  };
+
+  updateSaturation = event => {
+    console.log(event.target.value);
+    this.setState({
+      saturation: event.target.value
+    });
+  };
+
+  updateLightness = event => {
+    console.log(event.target.value);
+    this.setState({
+      lightness: event.target.value
+    });
+  };
+
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <h1>My Color Picker</h1>
+        <section
+          className="color"
+          style={{
+            backgroundColor: `hsl(${this.state.hue},${this.state.saturation}%,${
+              this.state.lightness
+            }%)`
+          }}
+        >
+          Color
+        </section>
+
+        <section className="slider">
+          <section className="hue">
+            Hue
+            <input
+              type="range"
+              className="hue"
+              name="hue"
+              min="0"
+              max="240"
+              onChange={this.updateHue}
+            />
+          </section>
+
+          <section className="Saturation">
+            Saturation
+            <input
+              type="range"
+              className="saturation"
+              name="saturation"
+              onChange={this.updateSaturation}
+            />
+          </section>
+
+          <section className="Lightness">
+            Lightness
+            <input
+              type="range"
+              className="lightness"
+              name="lightness"
+              onChange={this.updateLightness}
+            />
+          </section>
+        </section>
+        <button onClick={this.addColorToList}>Remember!</button>
+        <section className="list_container">
+          My Favorites
+          <ul>
+            {this.state.colors.map(color => {
+              return <li>{color}</li>;
+            })}
+          </ul>
+        </section>
+      </>
+    );
   }
 }
 
-export default App
+export default App;
+/*
+//<ul>
+ {this.state.colors.map(color => {
+  return <li>{color}</li> </ul>;}
+}) */
